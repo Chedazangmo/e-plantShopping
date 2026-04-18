8fa0820b5c6858b2bbf867a90c96c6dd80aa93ff
chedazangmo <02220137.cst@rub.edu.bt>
2026-04-18 09:09:16 -0400

    added reducers

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    let total=0;
+    cart.forEach(item=>{
+     let total=0;
+     cart.forEach(item =>{
+        const quantity=item.quantity;
+        const cost = parseFloat(item.cost.substring(1));
+        total +=cost*quantity;
+
+     });
+     return total;
+    })
  
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
-
-
 
   const handleIncrement = (item) => {
-  };
+    const newQuantity=item.quantity +1;
+    dispatch(updateQuantity({name:item.name,quantity:newQuantity}))
+     };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity>1){
+        const newQuantity=item.quantity -1;
+    }
+    dispatch(updateQuantity({name: item.name,quantity:newQuantity}));
+
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const cost =parseFloat(item.cost.substring(1));
+    return (cost * item.quantity);
   };
 
   return (
